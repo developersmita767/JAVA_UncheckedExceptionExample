4bbe1322ea8557bdbc3f92beb1170b97e9ef46c3
developersmita767 <156692415+developersmita767@users.noreply.github.com>
2024-07-07 23:51:59 +0530

    Add files via upload

---
+++ UncheckedExceptionExample.java
@@ -0,0 +1,12 @@
+//package com.example;
+
+public class UncheckedExceptionExample {
+
+	public static void main(String[] args) {
+		int x = 0;
+        int y = 10;
+        int z = y / x;
+        System.out.println(z);
+	}
+
+}
